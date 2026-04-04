e536ba44c88554ad49b7c239f99478dd9ccc7c8f
Akash-x1i <akashatcode@gmail.com>
2026-04-04 20:36:20 +0530

    verify room fixed

---
+++ includes/tailwind.php
@@ -0,0 +1,12 @@
+<script src="https://cdn.tailwindcss.com"></script>
+<script>
+tailwind.config = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Segoe UI', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+      },
+    },
+  },
+};
+</script>
